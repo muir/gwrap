26aad8af7ec07837eb25f355bb57a6735cbbf966
David Muir Sharnoff <github@dave.sharnoff.org>
2023-04-07 23:53:39 -0700

    add documentation

--- syncmap120.go
+++ syncmap120.go
@@ -2,19 +2,34 @@
 
 package gwrap
 
-// CompareMap is only available when compiling with go 1.20 and above
+// CompareMap is a wrapper of sync.Map. It uses generics so
+// that casting is not required. All methods directly
+// correspond to sync.Map methods. Do not copy a SyncMap
+// after you start using it.
+//
+// CompareMap is only available when compiling with go 1.20 and above.
+//
+// CompareMap requires that values be comparable which is not true
+// of SyncMap.
 type CompareMap[K comparable, V comparable] struct {
 	SyncMap[K, V]
 }
 
+// CompareAndDelete will delete a value from the map if the
+// current value matches the provided value.
 func (m *CompareMap[K, V]) CompareAndDelete(key K, old V) (deleted bool) {
 	return m.Map.CompareAndDelete(key, old)
 }
 
+// CompareAndSwap will replace a value in the map if the
+// current value matches the provided "old" value.
 func (m *CompareMap[K, V]) CompareAndSwap(key K, old V, new V) bool {
 	return m.Map.CompareAndSwap(key, old, new)
 }
 
+// Swap will exchange the provided value with the value in the
+// map.
+//
 // Swap is only available with go 1.20 and above
 func (m *SyncMap[K, V]) Swap(key K, value V) (previous V, loaded bool) {
 	var v any
